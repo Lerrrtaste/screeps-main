c6cedf9587347948ec104c2f2e72dc9bef6f700c
Laurenz Foglia <lerrrtaste@protonmail.com>
2023-03-10 16:30:03 +0100

    Add base and starter roles

---
+++ src/roles/base.ts
@@ -0,0 +1,116 @@
+// base role for all other creep roles
+// should handle all generic common tasks
+// like spawning, harvesting, upgrading, etc
+//
+// it provides a common interface for all other roles
+// including state machine and common tasks
+// the actual role logic is implemented in the derived classes
+// like builder, harvester, etc
+
+interface BaseRoleMemory {
+    creepIds: string[];
+}
+
+interface BaseCreepMemory extends CreepMemory {
+    state: string;
+    role: string;
+}
+
+class BaseRole {
+    // array of possible states (only basic, role specific states are implemented in the children)
+    name: string = 'base';
+    memory: BaseRoleMemory = {
+        creepIds: []
+    }
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    // get all creeps with this role
+    getCreeps(): Creep[] {
+        return _.filter(Game.creeps, (creep) => creep.memory.role == this.name);
+    }
+
+    // get all creeps with this role and state
+    getCreepsByState(state: string): Creep[] {
+        return _.filter(this.getCreeps(), (creep) => {
+            let creepMemory = <BaseCreepMemory>creep.memory;
+            return creepMemory.state == state;
+        });
+    }
+
+    // go to position
+    goTo(creep: Creep, pos: RoomPosition) {
+        const to = new RoomPosition(pos.x, pos.y, pos.roomName);
+        creep.moveTo(to);
+        creep.room.visual.line(creep.pos, pos, { color: 'red' });
+    }
+
+    // get all creeps with this role, and call childrens functions
+    runRole(room: Room) {
+        this.loadRoleMemory();
+
+        this.preRun(room);
+
+        for (let creep of this.getCreeps()) {
+            let creepMemory = <BaseCreepMemory>creep.memory;
+            if (!creepMemory.state) {
+                this.initCreep(creep, creepMemory);
+            } else {
+                this.runCreep(creep);
+            }
+        }
+
+        this.postRun(this.getCreeps());
+
+        this.saveRoleMemory();
+    }
+
+    // to be overridden by children
+    public initCreep(creep: Creep, creepMemory: BaseCreepMemory) {
+        console.log('Init creep not implemented for role ' + this.name);
+    };
+
+    public runCreep(creep: Creep) {
+        console.log('Run creep not implemented for role ' + this.name);
+    };
+
+    public preRun(room: Room) {
+        console.log('Pre run not implemented for role ' + this.name);
+    };
+
+    public postRun(creeps: Creep[]) {
+        console.log('Post run not implemented for role ' + this.name);
+    };
+
+    // load role memory from global memory
+    loadRoleMemory() {
+        // console.log('Loading memory for role ' + this.name);
+        if (!Memory.roles) {
+            Memory.roles = {};
+            console.log('Created memory for roles');
+        }
+        if (!Memory.roles[this.name]) {
+            Memory.roles[this.name] = this.memory;
+            console.log('Created memory for role ' + this.name);
+        }
+
+        this.memory = Memory.roles[this.name];
+        // update memory of all creeps with this role
+        this.memory.creepIds = this.getCreeps().map((creep) => creep.id);
+        // console.log('Updated memory for role ' + this.name);
+
+        // console.log('Loaded memory: ' + JSON.stringify(this.memory));
+    }
+
+    // save role memory to global memory
+    saveRoleMemory() {
+        // console.log('Saving memory for role ' + this.name);
+        Memory.roles[this.name] = this.memory;
+        // console.log('Saved memory: ' + JSON.stringify(this.memory));
+    }
+}
+
+
+export { BaseRole, BaseCreepMemory, BaseRoleMemory };
--- src/roles/starter.ts
+++ src/roles/starter.ts
@@ -1,1 +1,161 @@
-export const hello = "hi";
+// screeps start role
+
+import { BaseRole, BaseRoleMemory, BaseCreepMemory } from "./base";
+
+enum States {
+    IDLE = "idle",
+    HARVESTING = "harvesting",
+    SUPPLY_SPAWN = "supply_spawn",
+    SUPPLY_CONTROLLER = "supply_controller",
+}
+
+interface StarterCreepMemory extends BaseCreepMemory {
+    targetPos: RoomPosition;
+    targetSourceId: string;
+}
+
+interface StarterRoleMemory extends BaseRoleMemory {
+    activeSourcesIds: string[],
+    // per sourceid: dict with [x,y]: creepId
+    harvestSlots: { [sourceId: string]: { [pos: string]: string } },
+}
+
+export class StarterRole extends BaseRole {
+    constructor() {
+        super("starter");
+    }
+
+    // harvestingPositions: RoomPosition[] = [];
+    // activeSources: Source[] = [];
+
+    memory: StarterRoleMemory = { creepIds: [], activeSourcesIds: [], harvestSlots: {} };
+
+
+    // Init new Creeps
+    public override initCreep(creep: Creep, creepMemory: StarterCreepMemory) {
+        console.log("Starter: initCreep" + creep.name);
+        creepMemory.state = States.IDLE;
+    }
+
+    // Prepare Run
+    public override preRun(room: Room) {
+
+        // collect harvesting positions //TODO cache
+        const sources = room.find(FIND_SOURCES_ACTIVE);
+        const terrain = room.getTerrain();
+        for (const source of sources) {
+            if (this.memory.activeSourcesIds.indexOf(source.id) != -1) {
+                continue; // already added
+            }
+            console.log("Starter: found new source " + source.id);
+            this.memory.activeSourcesIds.push(source.id);
+            this.memory.harvestSlots[source.id] = {};
+
+            for (let x = -1; x <= 1; x++) {
+                for (let y = -1; y <= 1; y++) {
+                    if (x == 0 && y == 0) {
+                        continue;
+                    }
+                    if (terrain.get(source.pos.x + x, source.pos.y + y) != TERRAIN_MASK_WALL) {
+                        this.memory.harvestSlots[source.id][source.pos.x + x + "," + (source.pos.y + y)] = "";
+                    }
+                }
+            }
+        }
+    }
+
+    public override runCreep(creep: Creep) {
+        const creepMemory = creep.memory as StarterCreepMemory;
+
+        console.log("Starter: " + creep.name + " is " + creepMemory.state);
+        this.updateCreepState(creep, creepMemory);
+
+        switch (creepMemory.state) {
+            case States.IDLE:
+                this.runIdle(creep, creepMemory);
+                break;
+            case States.HARVESTING:
+                this.runHarvesting(creep, creepMemory);
+                break;
+            case States.SUPPLY_SPAWN:
+                this.runSupplySpawn(creep, creepMemory);
+                break;
+            case States.SUPPLY_CONTROLLER:
+                this.runSupplyController(creep, creepMemory);
+                break;
+        }
+    }
+
+    private updateCreepState(creep: Creep, creepMemory: StarterCreepMemory) {
+        switch (creepMemory.state) {
+            case States.IDLE:
+                const harvestSlots = this.memory.harvestSlots;
+                for (const sourceId of this.memory.activeSourcesIds) {
+                    for (const pos in harvestSlots[sourceId]) {
+                        if (harvestSlots[sourceId][pos] == "") {
+                            creepMemory.targetPos = new RoomPosition(parseInt(pos.split(",")[0]), parseInt(pos.split(",")[1]), creep.room.name);
+                            creepMemory.targetSourceId = sourceId;
+                            creepMemory.state = States.HARVESTING;
+                            console.log("Starter: " + creep.name + " found harvest slot " + pos);
+                            creep.say("back2work");
+                            this.memory.harvestSlots[sourceId][pos] = creep.id;
+                        }
+                    }
+                }
+                break;
+
+            case States.HARVESTING:
+                if (creep.store.getFreeCapacity() == 0) {
+                    creepMemory.state = States.SUPPLY_SPAWN;
+                    this.memory.harvestSlots[creepMemory.targetSourceId][creepMemory.targetPos.x + "," + creepMemory.targetPos.y] = "";
+                    creep.say("I'm FULL");
+                }
+                break;
+
+            case States.SUPPLY_SPAWN:
+            case States.SUPPLY_CONTROLLER:
+                if (creep.store.getUsedCapacity() == 0) {
+                    creepMemory.state = States.IDLE;
+                    creep.say("All done");
+                }
+                break;
+        }
+    }
+
+    private runIdle(creep: Creep, creepMemory: StarterCreepMemory) {
+        // Check for available harvest slots
+        creep.say("am bored");
+    }
+
+    private runHarvesting(creep: Creep, creepMemory: StarterCreepMemory) {
+        // harvest
+        if (!creepMemory.targetSourceId || !creepMemory.targetPos) {
+            console.error("Starter: " + creep.name + " has invalid target");
+            return;
+        }
+
+        let targetSource = Game.getObjectById(creepMemory.targetSourceId) as Source;
+        let targetPos = creepMemory.targetPos;
+
+        if (creep.harvest(targetSource) == ERR_NOT_IN_RANGE) {
+            this.goTo(creep, targetPos);
+        }
+    }
+
+    private runSupplySpawn(creep: Creep, creepMemory: StarterCreepMemory) {
+        if (creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            this.goTo(creep, Game.spawns["Spawn1"].pos);
+        }
+    }
+
+    private runSupplyController(creep: Creep, creepMemory: StarterCreepMemory) {
+        if (creep.upgradeController(creep.room.controller!) == ERR_NOT_IN_RANGE) {
+            this.goTo(creep, creep.room.controller!.pos);
+        }
+    }
+
+    public override postRun(creeps: Creep[]) {
+    }
+}
+
+export { StarterRoleMemory, StarterCreepMemory };
