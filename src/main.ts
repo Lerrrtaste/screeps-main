b00a072333d77f7a40cfd857d518810a0342641a
Laurenz Foglia <lerrrtaste@protonmail.com>
2023-03-12 00:45:31 +0100

    Add Todo Notes

--- src/main.ts
+++ src/main.ts
@@ -43,119 +43,78 @@ declare global {
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  // console.log(`Current game tick is ${Game.time}`);
-  // let targetCreepCount = 12;
-  // let creepCount = _.size(Game.creeps);
 
-  // if (creepCount < targetCreepCount && Game.spawns["Spawn1"].spawning == null) {
-  //   let newName = "Harvester_V2_" + Game.time;
-  //   // console.log("Spawning new harvester: " + newName);
-  //   Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE, WORK], newName);
-  // }
+  // Genreal Todos
+  // - memorry plumbing every x ticks (cleanup dead creeps, add new creeps, etc)
 
-  //TODO distributor and harvest roles, store in containers next to sources for pickup
-  // calculate the number of creeps per role
-  // memorry plumbing every x ticks (cleanup dead creeps, add new creeps, etc)
-
-  // TODO ROles
-  // - All in one starter (basically this)
-  // - Distributor (take from containers and put in storage)
+  // Roles
+  // - Distributor (take from containers carry to spawn, extensions, storage)
+  //   tries to keep all containers 1/2 full (overflow to storage, underflow from storage)
+  //   body focus: carry, move
+  //
   // - Harvester (take from source and put in container)
-  // - Builder (take from storage and build)
+  //   body focus: work
   //
-  // Ui to create creep classes (body + role)
+  // - Worker (take from nearest container and put in construction sites, repair sites, etc)
+  //   body focus: work, move,
   //
-  // ui targets for construction
+
+  // Distribution and Construction Logic
+  // - Supply Containers: x per source,
+  //   supplied by harvesters,
+  //   emptied by distributors
+  //
+  // - Consume Containers: x per source,
+  //   supplied by distributors,
+  //   used by workers
+  //
+  // - Storage: x per room, supplied by distributors,
+  //   supplied by distributers
+  //   take overflow of harvest containers
+  //   taken from if harvest containers are empty
+  //
+  // - Extensions: x per room, supplied by distributors
+  // - Spawn: x per room, supplied by distributors
+  //
+  // - Towers: x per room, supplied by distributors
+
+  // Resource Levels
+  // - Supply Container Energy
+  // - Consume Container Energy
+  // - Storage Energy
+  // - Spawn+Extension Energy
+  // - Harvestable Energy
+  // - Total Energy
+
+  
+  // Ui Actions
+  // - Create creepClasses
+  // - Set targets per creepClass
+  // - Set construciton targets
+  // - Set Worker Priorities
+  // - Set target Storage Levels (per container type)
+  //
+  // Ui Data
+  // - CreepClasses
+  // - Pop per Class
+  // - Pop per Role
+  // - Creeps (Name, Class, Role, state, (target), lifetime)
+  // - Construction Queue and Progress
+  // - Spawn Queue and Progress
+  // - Terrain
+  // - Harvesting slots (claimed/free/status)
+  //
   // phase system with room level
 
-  // test
   const starterClass = new CreepClass("Dan", 1, [WORK, CARRY, MOVE, WORK], "starter");
-  // starterClass.spawn(Game.spawns["Spawn1"]);
-
   let starterRole = new StarterRole();
   starterRole.runRole(Game.rooms["E6N39"]);
 
-  const targetPop = 8;
+  const targetPop = 12;
   const currentPop = _.size(Game.creeps);
   if (currentPop < targetPop) {
     starterClass.spawn(Game.spawns["Spawn1"]);
   }
-
-
-
-  // for (let name in Game.creeps) {
-  //   let creep = Game.creeps[name];
-  //   if (creep.memory.role == "harvester") {
-  //     // update working status
-  //     if (creep.memory.working == false && creep.store[RESOURCE_ENERGY] == 0) {
-  //       creep.memory.working = true;
-  //       creep.say("Going to work now");
-  //       creep.memory.target = creep.memory.target + 1;
-  //       creep.memory.target = creep.memory.target % 6;
-  //       // console.log(creep.name + " is going to work at " + source);
-  //       console.log("nextSource: " + creep.memory.target);
-  //     }
-  //     else if (creep.memory.working == true && creep.store[RESOURCE_ENERGY] == 50) {
-  //       creep.memory.working = false;
-  //       creep.say("I am full");
-  //     }
-
-  //     // do work
-  //     if (creep.memory.working) {
-  //       let sourceIdx = creep.memory.target < 5 ? 1 : 0;
-  //       let source = creep.room.find(FIND_SOURCES)[sourceIdx];
-  //       // if (typeof creep.memory.target != "number")
-  //       //   console.log("source is null");
-  //       //   creep.memory.working = false;
-  //       //   continue;
-  //       // }
-  //       if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-  //         creep.moveTo(source);
-  //         creep.room.visual.poly([creep.pos, source.pos], { stroke: '#ff0000' });
-  //       }
-  //     }
-  //     else {
-  //       if (Game.spawns["Spawn1"].store[RESOURCE_ENERGY] < 300) {
-  //         if (creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-  //           creep.moveTo(Game.spawns["Spawn1"]);
-  //           creep.room.visual.poly([creep.pos, Game.spawns["Spawn1"].pos], { stroke: '#ff0000', strokeWidth: 0.15, opacity: 0.1, lineStyle: 'dashed' });
-  //         }
-  //       } else {
-  //         // bring to controller
-  //         let controller = creep.room.controller;
-  //         if (controller) {
-  //           if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
-  //             creep.moveTo(controller);
-  //             creep.room.visual.poly([creep.pos, controller.pos], { stroke: '#ff0000', strokeWidth: 0.15, opacity: 0.1, lineStyle: 'dashed' });
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-
-
-
-
-
-
-  // for (let name in Game.creeps) {
-  //   let creep = Game.creeps[name];
-  //   if (true || creep.memory.role == "harvester") {
-  //     let sources = creep.room.find(FIND_SOURCES);
-  //     if (creep.store[RESOURCE_ENERGY] < 50 && creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-  //       creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
-  //       creep.say("🔄 harvest");
-  //     } else if (creep.store.getFreeCapacity() > 0) {
-  //       if(creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-  //         creep.moveTo(Game.spawns["Spawn1"], { visualizePathStyle: { stroke: "#ffffff" } });
-  //         creep.say("🔄 transfer");
-  //       }
-  //     }
-  //   }
-  // }
-
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
